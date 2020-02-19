93eaff92257b2afbe6d7300cb8c462bf23cbfa28
Michal Piechowiak <misiek.piechowiak@gmail.com>
2020-02-19 19:52:10 +0100

    make it break

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,6 +27,13 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: `gatsby-theme-blog`,
+      options: {
+        // basePath defaults to `/`
+        basePath: `/blog`,
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
